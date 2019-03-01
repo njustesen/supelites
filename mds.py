c823cbb121798923770b7f97c814469fd0d8411b
Niels Justesen <njustesen@gmail.com>
2019-03-01 16:13:53 +0100

    first commit

---
+++ mds.py
@@ -0,0 +1,233 @@
+from sklearn.manifold import MDS
+import os.path
+import matplotlib.pyplot as plt
+import json
+import pickle
+from sklearn.cluster import DBSCAN, SpectralClustering
+import numpy as np
+import matplotlib.cm as cm
+import seaborn as sns
+import numpy as np
+import matplotlib as mpl
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+import json
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+#D = np.ones((10000, 10000))
+n = 2881
+l = 14
+cache = True
+
+
+def readable_build_order(str_build_order):
+    build_order = []
+    for str_build in str_build_order:
+        build_order.append(builds[ord(str_build)])
+    return build_order
+
+
+def closest_point(point, points):
+    points = np.asarray(points)
+    dist_2 = np.sum((points - point)**2, axis=1)
+    return points[np.argmin(dist_2)]
+
+
+def plot_pca(data, build_orders, pca=False, eps=0.3, min_samples=10, min_clusters=1):
+    fig, plot = plt.subplots()
+    fig.set_size_inches(4, 4)
+    plt.prism()
+
+    #plot.scatter(data[...,0][:-5], data[...,1][:-5], c=all_colors[:-5])
+    #s = [100 for n in range(5)]
+    #plot.scatter(data[...,0][-5:], data[...,1][-5:], c=all_colors[-5:], marker='s', s=s)
+
+    # Clustering
+    db = DBSCAN(eps=eps, min_samples=min_samples).fit(data)
+
+    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+    core_samples_mask[db.core_sample_indices_] = True
+    labels = db.labels_
+
+    # Number of clusters in labels, ignoring noise if present.
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+    print('Estimated number of clusters: %d' % n_clusters_)
+    if n_clusters_ < min_clusters:
+        return
+
+    # Black removed and is used for noise instead.
+    unique_labels = set(labels)
+    colors = [plt.cm.Spectral(each)
+              for each in np.linspace(0, 1, len(unique_labels))]
+    for k, col in zip(unique_labels, colors):
+        if k == -1:
+            # Black used for noise.
+            col = [0, 0, 0, 1]
+
+        class_member_mask = (labels == k)
+
+        xy = data[class_member_mask & core_samples_mask]
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col), markersize=3, fillstyle='full', markeredgewidth=0.0)
+
+        # print builds in cluster
+        print("Cluster {} build orders".format(k))
+        for point in xy[:10]:
+            for i in range(len(data)):
+                if np.array_equal(data[i], point):
+                    print(readable_build_order(build_orders[i]))
+                    break
+
+        # Find centroids
+        if len(xy) > 0:
+            center = (sum(xy[:, 0]) / len(xy), sum(xy[:, 1]) / len(xy))
+            centroid = closest_point(center, xy)
+            centroid_idx = -1
+            assert centroid in data
+            for i in range(len(data)):
+                if np.array_equal(data[i], centroid):
+                    centroid_idx = i
+                    break
+            assert centroid_idx >= 0
+
+            print("Centroid of cluster {} with color {} and position {},{}".format(k, col, centroid[0], centroid[1]))
+            print(readable_build_order(build_orders[centroid_idx]))
+            plt.plot(centroid[0], centroid[1], 'o', markerfacecolor=tuple(col),
+                 markeredgecolor='k', markersize=8)
+
+            bbox_props = dict(boxstyle="circle,pad=0.1", fc="white", ec="black", lw=1)
+            t = plot.text(centroid[0], centroid[1], k, ha="center", va="center",
+                          rotation=0,
+                          size=6,
+                          bbox=bbox_props)
+
+        xy = data[class_member_mask & ~core_samples_mask]
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col), markersize=3, fillstyle='full', markeredgewidth=0.0)
+
+    plot.set_xticks(())
+    plot.set_yticks(())
+    '''
+    custom_lines = [Line2D([0], [0], marker='o', color='w', label='Scatter',
+                          markerfacecolor='red', markersize=4),
+                    Line2D([0], [0], marker='o', color='w', label='Scatter',
+                          markerfacecolor='white', markeredgecolor='black', markersize=4)]
+    '''
+    #for i in range(len(human_levels)):
+    #    plot.annotate("Level {}".format(i), (data[len(gen_levels)+i][0], data[len(gen_levels)+i][1]))
+    '''
+    for i in range(len(human_levels)):
+        bbox_props = dict(boxstyle="circle,pad=0.1", fc="white", ec="black", lw=2)
+        t = plot.text(data[len(gen_levels)+i][0], data[len(gen_levels)+i][1], i, ha="center", va="center", rotation=0,
+                    size=15,
+                    bbox=bbox_props)
+    '''
+    plt.tight_layout(pad=-0.5, w_pad=-0.5, h_pad=-0.5)
+    #plot.margins(0, 0)
+    #lines = ax.plot(data)
+    title = "PCA" if pca else "t-SNE"
+    #plt.title(title)
+    #plot.legend(custom_lines, ['PCG', 'Human'], loc=1)
+    #plot.legend(['Won', 'Lost', 'Human'], loc=2)
+    fig.savefig("plots/mds/{}_eps-{}_sam-{}.pdf".format("pca" if pca else "t-sne", eps, min_samples), bbox_inches='tight', pad_inches=0)
+    return fig
+
+
+def norm(x, min_x, max_x):
+    return (x - min_x) / (max_x - min_x)
+
+
+def plot_pca_mmr(data, build_orders, mmrs):
+    fig, plot = plt.subplots()
+    fig.set_size_inches(4, 4)
+    plt.prism()
+    x = data[:, 0]
+    y = data[:, 1]
+    min_mmr = np.min(mmrs)
+    max_mmr = np.max(mmrs)
+    c = cm.rainbow([norm(mmr, min_mmr, max_mmr) for mmr in mmrs])
+    for i in range(len(data)):
+        plt.scatter(x[i], y[i], color=c[i], s=2)
+
+    plot.set_xticks(())
+    plot.set_yticks(())
+
+    plt.tight_layout(pad=-0.5, w_pad=-0.5, h_pad=-0.5)
+    fig.savefig("plots/mds/mmr.pdf".format(),
+                bbox_inches='tight', pad_inches=0)
+    return fig
+
+
+def plot_histogram(data, build_orders, mmrs, num_bins=30):
+
+    plt.close('all')
+    plt.style.use('ggplot')
+    fig = plt.figure(figsize=(8, 5))
+    plt.xlabel('')
+    plt.ylabel('Count')
+    #plt.title(feature.title)
+    plt.xlim(-2000, 2000)
+    # plt.tight_layout()
+    n, bins, patches = plt.hist(data, num_bins, density=False, alpha=0.75)
+    norm = mpl.colors.Normalize(vmin=2000, vmax=5000)
+    cmap = 'viridis'
+    for i in range(len(patches)):
+        patch = patches[i]
+        ran = [patch.xy[0] - patch._width/2, patch.xy[0] + patch._width/2]
+        # data_in_patch = [data[i] for i in range(len(data)) if ran[0] <= data[i][0] <= ran[1]]
+        build_orders_in_patch = [build_orders[i] for i in range(len(data)) if ran[0] <= data[i][0] <= ran[1]]
+        mmrs_in_patch = [mmrs[i] for i in range(len(data)) if ran[0] <= data[i][0] <= ran[1]]
+        m = cm.ScalarMappable(norm=norm, cmap=cmap)
+        mean_mmr = np.mean(mmrs_in_patch)
+        rgb = m.to_rgba(mean_mmr)
+        print(mean_mmr)
+        patch.set_facecolor(rgb)
+        print("Patch {} [{} - {}]:".format(i, ran[0], ran[1]))
+        for bo in build_orders_in_patch:
+            print(readable_build_order(bo))
+    #cbax = fig.add_axes([0.85, 0.11, .04, .78])
+    divider = make_axes_locatable(plt.gca())
+    cax = divider.append_axes("right", "5%", pad="3%")
+    cb1 = mpl.colorbar.ColorbarBase(cax, cmap=cmap,
+                                    norm=norm,
+                                    orientation='vertical')
+    cb1.set_label('MMR')
+    #plt.grid(True)
+
+    plt.savefig('plots/mds_1d.pdf')
+    plt.clf()
+
+
+build_orders = pickle.load(open('data/distance_matrix/TvZ_build_orders_{}_{}.p'.format(n, l), 'rb'))
+mmrs = pickle.load(open('data/mmr/TvZ_mmr_{}.p'.format(n), 'rb'))
+player_ids = pickle.load(open('data/player_ids/TvZ_playerid_{}.p'.format(n), 'rb'))
+
+dims = [1, 2]
+for dim in dims:
+    if not (cache and os.path.isfile('data/transformations/trans{}_{}_{}.p'.format(dims, n, l))):
+        D = pickle.load(open('data/distance_matrix/TvZ_{}_{}.p'.format(n, l), 'rb'))
+        embedding = MDS(n_components=dims, dissimilarity='precomputed')
+        transformed = embedding.fit_transform(D)
+        out = {
+            'embedding': embedding,
+            'transformed': transformed,
+            'build_orders': build_orders,
+            'player_ids': player_ids,
+            'mmrs': mmrs
+        }
+        print(transformed.shape)
+        pickle.dump(out, open('data/transformations/trans_{}_{}_{}.p'.format(dims, n, l), 'wb'))
+
+    # Load cost table
+    print('Parsing cost table')
+    with open('data/costs/T_costs.json', newline='') as file:
+        s = file.read()
+        file.close()
+        costs = json.loads(s)
+    builds = [build for build in costs.keys()]
+
+    transformed = pickle.load(open('data/transformations/trans_{}_{}_{}.p'.format(dim, n, l), 'rb'))
+    if dim == 2:
+        plot_pca_mmr(transformed['transformed'], transformed['build_orders'], transformed['mmrs'])
+        plot_pca(transformed['transformed'], transformed['build_orders'], transformed['mmrs'], eps=120, min_samples=25)
+    elif dim == 1:
+        for bins in [10, 25, 50, 100]:
+            plot_histogram(transformed['transformed'], transformed['build_orders'], transformed['mmrs'], num_bins=bins)
---
+++ w_levenshtein.py
@@ -0,0 +1,124 @@
+import numpy as np
+import json
+import pickle
+from weighted_levenshtein import lev, osa, dam_lev
+import numpy as np
+import json
+
+ascii_index = 0
+
+# Load replay data
+print('Parsing player data')
+with open('data/players/TvZ_player_info.json', newline='') as file:
+    s = file.read()
+    file.close()
+    players = json.loads(s)
+
+# Load cost table
+print('Parsing cost table')
+with open('data/costs/T_costs.json', newline='') as file:
+    s = file.read()
+    file.close()
+    costs = json.loads(s)
+
+keys = list(costs.keys())
+insert_costs = np.ones(len(keys)+ascii_index, dtype=np.float64)
+delete_costs = np.ones(len(keys)+ascii_index, dtype=np.float64)
+substitute_costs = np.ones((len(keys)+ascii_index, len(keys)+65), dtype=np.float64)
+transpose_costs = np.ones((len(keys)+ascii_index, len(keys)+65), dtype=np.float64)
+for i in range(len(costs)):
+    build = keys[i]
+    cost_i = costs[build]['minerals']+costs[build]['vespene']
+    insert_costs[i+ascii_index] = cost_i
+    delete_costs[i+ascii_index] = cost_i
+    for j in range(len(costs)):
+        cost_j = costs[keys[j]]['minerals'] + costs[keys[j]]['vespene']
+        substitute_costs[i+ascii_index][j+ascii_index] = cost_i + cost_j
+    for j in range(len(costs)):
+        cost_j = costs[keys[j]]['minerals'] + costs[keys[j]]['vespene']
+        transpose_costs[i+ascii_index][j+ascii_index] = min(cost_i, cost_j)
+
+# Load build orders
+print('Parsing build orders')
+with open('data/build_orders/TvZ_build_orders.json', newline='') as file:
+    s = file.read()
+    file.close()
+    build_orders = json.loads(s)
+
+#print(build_orders)
+n = len(build_orders)
+#n = 3000
+l = 16
+
+'''
+int_build_orders = []
+for build_order in list(build_orders.values())[:n]:
+    int_build_order = []
+    l_b = min(l, len(build_order))
+    for build in build_order[:l_b]:
+        int_build_order.append(keys.index(build))
+    int_build_orders.append(int_build_order)
+'''
+
+str_build_orders = []
+mmrs = []
+for player_id, build_order in list(build_orders.items())[:n]:
+    mmr = players[player_id]['player_mmr']
+    if len(build_order) < l or mmr < 0:
+        continue
+    mmrs.append(mmr)
+    str_build_order = ""
+    l_b = min(l, len(build_order))
+    sorted_build_order = {int(k): v for k, v in build_order.items()}
+    for frame in sorted(list(sorted_build_order.keys()))[:l_b]:
+        build = sorted_build_order[frame]
+        if costs[build]['minerals'] + costs[build]['vespene'] > 0:
+            c = chr(keys.index(build) + ascii_index)
+            str_build_order += c
+    if str_build_order != "":
+        str_build_orders.append(str_build_order)
+        print(str_build_order)
+
+print(sorted(str_build_orders))
+n = len(str_build_orders)
+
+D = np.zeros((n, n))
+
+'''
+i = 0
+for player_id, build_order in build_orders.items():
+    if 100 <= i <= 110:
+        print(player_id)
+        print(build_order[:8])
+    i += 1
+'''
+
+pickle.dump(str_build_orders, open("data/distance_matrix/TvZ_build_orders_{}_{}.p".format(n, l), "wb"))
+pickle.dump(mmrs, open("data/mmr/TvZ_mmr_{}.p".format(n), "wb"))
+
+i = 0
+for y in range(n):
+    print(i, "/", n)
+    for x in range(n):
+        str_build_orders[x] = str_build_orders[x]
+        str_build_orders[y] = str_build_orders[y]
+        if y == x:
+            distance = 0
+        else:
+            distance = dam_lev(str1=str_build_orders[x],
+                               str2=str_build_orders[y],
+                               insert_costs=insert_costs,
+                               delete_costs=delete_costs,
+                               substitute_costs=substitute_costs,
+                               transpose_costs=transpose_costs
+                               )
+        distance = int(distance)
+        D[y][x] = distance
+        D[x][y] = distance
+        #print(str_build_orders[x])
+        #print(str_build_orders[y])
+        #print(distance)
+    i += 1
+
+print("Storing distance matrix")
+pickle.dump(D, open("data/distance_matrix/TvZ_{}_{}.p".format(n, l), "wb"))
